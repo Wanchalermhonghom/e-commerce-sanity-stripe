d2c61c6d954ab25dd87f54232df7c04efa7b70de
Wanchalermhonghom <wanchalerm.honghom@gmail.com>
2023-06-02 15:36:57 +0200

    add detailpage

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
 import {
   Card,
@@ -9,8 +11,10 @@ import {
 } from '@/components/ui/card';
 import { urlForImage } from '@/sanity/lib/image';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 type ProductProps = {
+  _id: string;
   title: string;
   subtitle: string;
   description: string;
@@ -20,10 +24,14 @@ type ProductProps = {
   quantity: number;
 };
 
-function ProductCard({ title, subtitle, price, image }: ProductProps) {
+function ProductCard({ _id, title, subtitle, price, image }: ProductProps) {
   const urlPath = urlForImage(image).url();
+  const router = useRouter();
   return (
-    <Card className="w-[267px]">
+    <Card
+      className="w-[267px] hover:scale-105 cursor-pointer transition-transform"
+      onClick={() => router.push('/product/' + _id)}
+    >
       <Image src={urlPath} alt="productImage" width={267} height={334} />
       <CardHeader>
         <CardTitle>{title}</CardTitle>
