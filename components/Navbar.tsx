29008fd3660e6e5b47106b3faf223a73406e80be
Wanchalermhonghom <wanchalerm.honghom@gmail.com>
2023-06-02 12:23:18 +0200

    add productlist filter section

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,12 +5,17 @@ import {
   NavigationMenuItem,
   NavigationMenuList,
 } from '@/components/ui/navigation-menu';
+import Image from 'next/image';
 
 export default function Navbar() {
   return (
-    <NavigationMenu className='bg-slate-200'>
-      <NavigationMenuList>
-        <NavigationMenuItem>Logo</NavigationMenuItem>
+    <NavigationMenu>
+      <NavigationMenuList className="flex justify-between">
+        <div>Link</div>
+        <NavigationMenuItem>
+          <Image alt="logo" src={'/logo.png'} height={100} width={100} />
+        </NavigationMenuItem>
+        <NavigationMenuItem>Cart</NavigationMenuItem>
       </NavigationMenuList>
     </NavigationMenu>
   );
--- components/ProductListing.tsx
+++ components/ProductListing.tsx
@@ -1,5 +1,7 @@
 import ProductCard from '@/components/ProductCard';
 import { Product } from '@/services/product-service';
+import { ProductFilter } from './ProductFilter';
+import Search from './Search';
 
 type ProductListingProps = {
   products: Product[];
@@ -7,7 +9,12 @@ type ProductListingProps = {
 
 function ProductListing({ products }: ProductListingProps) {
   return (
-    <section className="p-24">
+    <section className="p-24 flex flex-col gap-4">
+      <div className="flex justify-end gap-2">
+        <Search></Search>
+
+        <ProductFilter></ProductFilter>
+      </div>
       <div className="flex flex-wrap gap-4">
         {products.map((product: Product) => (
           <ProductCard key={product._id} {...product} />
